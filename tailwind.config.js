bcaf777c85a1b205f0e0ba46f92719f60d302e71
ayashadevikar <ayashadevikar8@gmail.com>
2023-07-14 13:05:47 +0530

    rode clone  added

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'dark-orange': '#ad9970',
+      'light-gray': '#808080',
+      'black': '#000000',
+      'dark-gray': '#484848',
+      // #6c757d
+      //
+    //  #343a40
+    
+      'gray': '#6c757',
+     
+            
+    },
+    
+    extend: {
+      screens: {
+        'md': '640px',
+      },
+  },
+  plugins: [],
+}
+}
